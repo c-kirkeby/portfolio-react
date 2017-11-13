a2a3886b2390727a77ed0dea969c59d3b3c90bf3
JheeBz <Christian_kirkeby@hotmail.com>
2017-11-13 11:11:39 +0000

    Attempted to fix Bottom Nav

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -5,11 +5,12 @@ import {
 
 const BottomNav = () => {
   return (
-    <footer className="align-middle">
+    <footer className="BottomNav">
       <Container>
         <Row>
           <Col xs="12" md="12" className="text-center">
-            <span className="text-muted created-by">Created by Christian Kirkeby using ReactJS</span>
+            <span className="text-muted created-by">Created by Christian Kirkeby</span>
+            {/* <span className="text-muted created-by align-middle">Created by Christian Kirkeby using <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">ReactJS</a> & Bootstrap 4</span> */}
           </Col>
         </Row>
       </Container>
