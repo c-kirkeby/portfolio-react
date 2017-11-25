bde42b4ae82dfd0e473d1cdec392267120187887
JheeBz <Christian_kirkeby@hotmail.com>
2017-11-25 01:09:46 +0000

    Added flair and social media to Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,10 +9,22 @@ const Footer = () => {
       <Container>
         <Row>
           <Col xs="12" md="12" className="text-center">
-            <span className="text-muted created-by">Created by Christian Kirkeby</span>
-            {/* <span className="text-muted created-by">Created by Christian Kirkeby using <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">ReactJS</a> & Bootstrap 4</span> */}
+            <span className="text-muted created-by">Created by Christian Kirkeby using&nbsp;
+              <a href="https://getbootstrap.com"><i className="devicon-bootstrap-plain" alt="Bootstrap 4"></i></a> &&nbsp;
+              <a href="https://reactjs.org/"><i className="devicon-react-plain" alt="ReactJS"></i></a> |&nbsp;
+              <a href="https://www.linkedin.com/in/christian-kirkeby-29912352/" alt="LinkedIn"><i className="fa fa-2x fa-linkedin"></i></a>&nbsp;
+              <a href="https://github.com/jheebz" alt="GitHub"><i className="fa fa-2x fa-github"></i></a>
+            </span>
           </Col>
         </Row>
+        {/* <Row>
+          <Col xs="12" md="12" className="text-center">
+            <span className="created-by">
+              <a href="https://www.linkedin.com/in/christian-kirkeby-29912352/" alt="LinkedIn"><i className="fa fa-2x fa-linkedin"></i></a>&nbsp;
+              <a href="https://github.com/jheebz" alt="GitHub"><i className="fa fa-2x fa-github"></i></a>
+            </span>
+          </Col>
+        </Row> */}
       </Container>
     </footer>
   )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-// import 'devicons/';
 import './assets/css/fonts.css';
 import './assets/css/index.css';
+import 'font-awesome/css/font-awesome.min.css';
 import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
 
