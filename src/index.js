044accad56389a63302994bcdd8ebdcf4de0fae9
ckirkeby <christian@christiankirkeby.com>
2018-06-20 21:09:26 +1000

    Added Loadable to render page before displaying

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Loadable from 'react-loadable';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/css/fonts.css';
 import './assets/css/index.css';
 import 'font-awesome/css/font-awesome.min.css';
-import App from './App';
+// import App from './App';
 // import registerServiceWorker from './registerServiceWorker';
+
+const App = Loadable({
+  loader: () => import('./App'),
+  loading() {
+    return <div></div>
+  }
+})
 
 ReactDOM.render((
     <App />
